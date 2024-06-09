87cd3485a39e5d169c319b07d8b631ef0a79ff16
moxer-mmh <moncefho10@gmail.com>
2024-06-09 23:29:36 +0100

    I have added the landing page components and removed the default react components also added the Footer component and styled it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import './App.css';
+import Footer from "./Pages/LandingPage/Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          <a
-            className="App-link"
-            href="https://github.com/moxer-mmh"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            My Github
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
 
