d473985c70ecbc1f2ca1bd0a75a5563d384da0f5
moxer-mmh <moncefho10@gmail.com>
2024-06-09 23:36:24 +0100

    i have added the Header and the body Components but without there codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Footer from "./Pages/LandingPage/Components/Footer/Footer";
+import Body from "./Pages/LandingPage/Components/Body/Body";
+import Header from "./Pages/LandingPage/Components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
+      <Body />
       <Footer />
     </>
   );
---
+++ src/Pages/LandingPage/Components/Body/Body.jsx
@@ -0,0 +1,9 @@
+function Body() {
+  return (
+    <div>
+      <h1>Body</h1>
+    </div>
+  );
+}
+
+export default Body;
