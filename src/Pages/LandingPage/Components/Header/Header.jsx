dc0d3f66a0d5adfac71cbe22efd3aa680bf76c27
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-06-10 00:38:30 -0400

    I did the navigation bar. Also I added two pictures that i used. I did the teamWork section.

--- src/Pages/LandingPage/Components/Header/Header.jsx
+++ src/Pages/LandingPage/Components/Header/Header.jsx
@@ -1,21 +1,54 @@
+import "./Header.css";
+import logoReact from "../../Assets/logo192.png";
+import teamWork from "../../Assets/teamWork.png";
+
 function Header() {
   return (
     <header className="Header">
-      <nav>nav</nav>
-      <div>
-        <div>
-          <img alt="Team Work" />
+      <div className="navBar">
+        <div className="titre">
+          <img src={logoReact} alt="" className="logoReact" />
         </div>
-        <section>
-          <h1>
-            Bridging teams together,<br></br> one message at a time
-          </h1>
-          <p>
-            Stay connected and collaborate in real-time across web, mobile, and
-            desktop with our all-in-one platform.
-          </p>
-          <button>Get Started</button>
-        </section>
+        <nav className="liens">
+          <a href="#" className="texteLiens">
+            {" "}
+            Home{" "}
+          </a>
+          <a href="#" className="texteLiens">
+            {" "}
+            Blog{" "}
+          </a>
+          <a href="#" className="texteLiens">
+            {" "}
+            News{" "}
+          </a>
+          <a href="#" className="texteLiens">
+            {" "}
+            Feedbacks{" "}
+          </a>
+        </nav>
+        <div>
+          <button className="log">Se connecter</button>
+        </div>
+      </div>
+
+      <div className="teamWork">
+          <section className="laSection">
+            <div className="titleSec">
+              Bridging teams together. one message at a time
+            </div>
+            <p>
+              Stay connected and collaborate in real-time across web, mobile,
+              and desktop with our all-in-one platform.
+            </p>
+            <button className="getStarted">Get Started</button>
+
+          </section>
+
+          <div>
+            <img src={teamWork} alt="Team Work" />
+          </div>
+        
       </div>
     </header>
   );
