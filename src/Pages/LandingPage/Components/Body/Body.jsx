25be018c352c1cbca095cffa47c0a0f6a5e91bd0
moxer-mmh <moncefho10@gmail.com>
2024-06-10 22:50:19 +0100

    i added a the news section in the body to the landing page and styled it

--- src/Pages/LandingPage/Components/Body/Body.jsx
+++ src/Pages/LandingPage/Components/Body/Body.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Body.css";
 import Checkmark from "../../Assets/checkmark--outline.svg";
+import TagCloud from "TagCloud";
 
 function Body() {
   const [feedback, setFeedback] = useState("");
   const [name, setName] = useState("");
   const [isPopupVisible, setIsPopupVisible] = useState(false);
 
+  useEffect(() => {
+    return () => {
+      const container = ".tagcloud";
+      const texts = [
+        "Comunication",
+        "Project",
+        "Document",
+        "Collaboration",
+        "Management",
+        "Editing",
+        "Chat",
+        "Features",
+        "Real-time",
+        "Team",
+        "Review",
+        "Task",
+        "File",
+        "Integration",
+        "Workflow",
+      ];
+      const options = {
+        radius: 300,
+        maxSpeed: "normal",
+        initSpeed: "normal",
+        keep: true,
+      };
+      TagCloud(container, texts, options);
+    };
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     //here to add feedback to the data base but not now
     setIsPopupVisible(true);
-
   };
 
   const handleClosePopup = () => {
@@ -23,7 +53,11 @@ function Body() {
   return (
     <div className="Body">
       <div>Blog</div>
-      <div>News</div>
+      <div className="News">
+        <div className="news-container">
+          <span className="tagcloud"></span>
+        </div>
+      </div>
       <div className="feedback">
         <form onSubmit={(e) => handleSubmit(e)}>
           <fieldset className="feedback-container">
