56f2d9b76b817d348979fea39bd9de67a479f6e4
moxer-mmh <moncefho10@gmail.com>
2024-06-10 20:57:56 +0100

    adding the HomeButton and the feedback place i added to the LandingPage a HomeButton component that will be used to go up to the head of the page when clicked also i styled it . i added the feedback place in the body of the landing page an styled it also.

--- src/Pages/LandingPage/Components/Body/Body.jsx
+++ src/Pages/LandingPage/Components/Body/Body.jsx
@@ -1,9 +1,61 @@
+import React, { useState } from "react";
+import "./Body.css";
+import Checkmark from "../../Assets/checkmark--outline.svg";
+
 function Body() {
+  const [feedback, setFeedback] = useState("");
+  const [name, setName] = useState("");
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //here to add feedback to the data base but not now
+    setIsPopupVisible(true);
+
+    setFeedback("");
+    setName("");
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupVisible(false);
+  };
+
   return (
     <div className="Body">
       <div>Blog</div>
       <div>News</div>
-      <div>Feedbacks</div>
+      <div className="feedback">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <fieldset className="feedback-container">
+            <h2 className="introduction">Leave a Feedback</h2>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Name"
+              className="feedback-input-name"
+            />
+            <br />
+            <textarea
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+              placeholder="Write your feedback here..."
+              className="feedback-textarea"
+            />
+            <button type="submit" className="feedback-button">
+              Submit
+            </button>
+          </fieldset>
+        </form>
+      </div>
+
+      {isPopupVisible && (
+        <div className="popup">
+          <img src={Checkmark} alt="Checkmark" />
+          <h3>Thanks for leaving a recommendation!</h3>
+          <button onClick={handleClosePopup}>Ok</button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Pages/LandingPage/Components/HomeButton/HomeButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Arrow from '../../Assets/arrow-up-solid.svg';
+import './HomeButton.css'; // Import the CSS file
+
+const HomeButton = () => {
+  return (
+    <div className="iconbutton">
+      <a href="">
+        <img src={Arrow} alt="home" />
+      </a>
+    </div>
+  );
+}
+
+export default HomeButton;
