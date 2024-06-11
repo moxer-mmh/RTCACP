a25e8be936868a1d23101620bae9eae307406163
moxer-mmh <moncefho10@gmail.com>
2024-06-12 00:31:09 +0100

    modified:   src/App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
+// eslint-disable-next-line no-undef
 test("renders app component", () => {
   render(<App />);
 });
