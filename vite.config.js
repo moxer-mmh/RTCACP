8e86e366da8454a4c128bd82b6dcfda45fd5b275
moxer_mmh <45440270+moxer-mmh@users.noreply.github.com>
2024-06-12 06:30:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "//",
+  base: "/Real-Time-Communication-and-Collaboration-Platform/",
   build: {
     outDir: 'dist'
   },
