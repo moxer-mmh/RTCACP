e362f330fca4ce26fd103ce33301374e0690d488
moxer-mmh <moncefho10@gmail.com>
2024-06-18 12:39:31 +0100

    modified:   src/App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, screen, test, expect } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
-// eslint-disable-next-line no-undef
 test("renders app component", () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  // Assuming there is some text "learn react" to test for
+  const linkElement = screen.getByText(/"Bridging teams together"/i);
+  expect(linkElement).toBeInTheDocument();
 });
