8ce354a71c6d8d48d87c6cbb42e1115b6914bbad
moxer-mmh <moncefho10@gmail.com>
2024-07-01 15:28:41 +0100

    i used the useAxiosFetch hook to fetch the data from the server in Auth.jsx

--- src/Components/Pages/Auth/Auth.jsx
+++ src/Components/Pages/Auth/Auth.jsx
@@ -1,26 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Auth.css";
 import api from "../../../api/accounts";
 import Arrow from "../../../assets/arrow-left-solid.svg";
+import useAxiosFetch from "../../../Hooks/useAxiosFetch";
 
 function Auth() {
   const [isSignUp, setIsSignUp] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-  const [accounts, setAccounts] = useState([]);
-
-  useEffect(() => {
-    const fetchAccounts = async () => {
-      try {
-        const response = await api.get("/accounts");
-        setAccounts(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchAccounts();
-  }, []);
+  const {
+    data: accounts,
+    fetchError,
+    isLoading,
+  } = useAxiosFetch(`${api.defaults.baseURL}/accounts`);
 
   function handleSubmitSignIn(e) {
     e.preventDefault();
@@ -62,8 +55,7 @@ function Auth() {
     } else {
       api
         .post("/accounts", data)
-        .then((response) => {
-          setAccounts([...accounts, response.data]);
+        .then(() => {
           alert("Account created successfully");
         })
         .catch((error) => {
@@ -88,62 +80,66 @@ function Auth() {
 
   return (
     <main className="auth">
-      <div className={`auth-container ${isSignUp ? "signing-up" : ""}`}>
-        <div className="container signin-container">
-          <div className="signin">
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmitSignIn}>
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit">Sign In</button>
-            </form>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && fetchError && <p style={{ color: "red" }}>{fetchError}</p>}
+      {!isLoading && !fetchError && (
+        <div className={`auth-container ${isSignUp ? "signing-up" : ""}`}>
+          <div className="container signin-container">
+            <div className="signin">
+              <h1>Sign In</h1>
+              <form onSubmit={handleSubmitSignIn}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Sign In</button>
+              </form>
+            </div>
+            <div className="signup-msg">
+              <p>Dont have an account?</p>
+              <button onClick={handleSignUp}>Sign Up</button>
+            </div>
           </div>
-          <div className="signup-msg">
-            <p>Dont have an account?</p>
-            <button onClick={handleSignUp}>Sign Up</button>
+          <div className="container signup-container">
+            <div className="signin-msg">
+              <p>Already have an account?</p>
+              <button onClick={handleSignIn}>Sign In</button>
+            </div>
+            <div className="signup">
+              <h1>Sign Up</h1>
+              <form onSubmit={handleSubmitSignUp}>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Sign Up</button>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="container signup-container">
-          <div className="signin-msg">
-            <p>Already have an account?</p>
-            <button onClick={handleSignIn}>Sign In</button>
-          </div>
-          <div className="signup">
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmitSignUp}>
-              <input
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit">Sign Up</button>
-            </form>
-          </div>
-        </div>
-      </div>
+      )}
       <div className="iconbutton">
         <a href="/">
           <img src={Arrow} alt="home" />
