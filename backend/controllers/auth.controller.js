0a2e561d15855aa1c7977e7ba29c7f9d429ea50a
moxer-mmh <moncefho10@gmail.com>
2024-07-20 10:20:49 +0100

    i added the login and the logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -77,12 +77,44 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("login route");
-  res.send("login route");
+export const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!isPasswordCorrect && user) {
+      return res.status(400).json({ error: "Invalid password." });
+    }
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    generateTokenAndCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error on login", error.message);
+    res.status(500).json({ error: "Something went wrong." });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log("logout route");
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.error("Error on logout", error.message);
+    res.status(500).json({ error: "Something went wrong." });
+  }
 };
