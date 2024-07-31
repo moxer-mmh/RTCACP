19dc47a18f8652f0a457595c339731d3ca86bd4b
moxer-mmh <moncefho10@gmail.com>
2024-07-31 18:15:47 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -14,6 +15,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 connectDB();
 
@@ -23,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
